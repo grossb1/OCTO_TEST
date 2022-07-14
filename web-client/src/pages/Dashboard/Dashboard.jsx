ca9f67e3557718827a5c768bce2434d6c1f2cf7c
Bradley Gross <bradley.gross@va.gov>
2022-07-14 16:49:15 -0700

    added vitest framework

--- web-client/src/pages/Dashboard/Dashboard.jsx
+++ web-client/src/pages/Dashboard/Dashboard.jsx
@@ -4,36 +4,21 @@ import { useQuery } from 'react-query';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import CircularProgress from '@mui/material/CircularProgress';
-import Alert from '@mui/material/Alert';
-import GlobalContext from '../contexts/GlobalContext';
-import Announcement from '../components/Announcements';
-import DynamicDataGrid from '../components/DynamicDataGrid';
-import NewSetMenuButton from '../components/NewSetMenuButton';
-import QueryTemplate from '../utility/Query';
+import GlobalContext from '../../contexts/GlobalContext';
+import Announcement from '../../components/Announcements';
+import DynamicDataGrid from '../../components/DynamicDataGrid';
+import NewSetMenuButton from '../../components/NewSetMenuButton';
+import QueryTemplate from '../../utility/Query';
+import columns from './config';
 
 const get = async () => {
-  const { data } = await axios.get(`http://localhost:5117/allsets`);
+  const { data } = await axios.get('http://localhost:5117/allsets');
   return data;
 };
 
 function Dashboard() {
   const query = useQuery('allsets', get);
   const { role } = useContext(GlobalContext);
-
-  if (query.isLoading) {
-    return (
-      <Box sx={{ display: 'flex' }}>
-        <CircularProgress />
-      </Box>
-    );
-  }
-
-  if (query.error) {
-    return (
-      <Alert variant="filled" severity="error">ERROR: Please contact system administrators.</Alert>
-    );
-  }
 
   // setting column header as the field name
   // const columns = [];
@@ -44,23 +29,6 @@ function Dashboard() {
 
   // column header is the field name
   // example of manually setting column header
-  const columns = [
-    {
-      field: 'setTerm', headerName: 'Set Term', width: 200,
-    },
-    {
-      field: 'setType', headerName: 'Set Type', width: 200,
-    },
-    {
-      field: 'organization', headerName: 'Organization', width: 200,
-    },
-    {
-      field: 'description', headerName: 'Description', width: 200,
-    },
-    {
-      field: 'status', headerName: 'Status', width: 200,
-    },
-  ];
 
   role.permissions.map((p) => {
     if (p === 'Dashboard Edit') {
@@ -74,7 +42,7 @@ function Dashboard() {
     return p;
   });
 
-  const rows = query.data;
+  const { data } = query;
 
   const uniqueID = (row) => row.guid;
   const announcement = 'Auto populated with a message about down time if any of the Lib tables used populate it is null or XLA cannot connect to SQL 20 otherwise blank.';
@@ -112,8 +80,7 @@ function Dashboard() {
         <NewSetMenuButton />
       </Box>
       <QueryTemplate query={query}>
-
-        <DynamicDataGrid rows={rows} columns={columns} uniqueID={uniqueID} />
+        <DynamicDataGrid rows={data} columns={columns} uniqueID={uniqueID} />
       </QueryTemplate>
     </>
 
---
+++ web-client/src/pages/Dashboard/config.jsx
@@ -0,0 +1,17 @@
+export default [
+  {
+    field: 'setTerm', headerName: 'Set Term', width: 200,
+  },
+  {
+    field: 'setType', headerName: 'Set Type', width: 200,
+  },
+  {
+    field: 'organization', headerName: 'Organization', width: 200,
+  },
+  {
+    field: 'description', headerName: 'Description', width: 200,
+  },
+  {
+    field: 'status', headerName: 'Status', width: 200,
+  },
+];
